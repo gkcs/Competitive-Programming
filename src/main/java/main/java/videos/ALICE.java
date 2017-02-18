92e4aa7d3a61c688766a131898acc67640b1c11c
gaurav.se <gauravsen92@gmail.com>
2017-02-18 16:41:15 +0530

    ACO written

--- src/main/java/main/java/videos/ALICE.java
+++ src/main/java/main/java/videos/ALICE.java
@@ -2,7 +2,10 @@ package main.java.videos;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class ALICE {
@@ -44,21 +47,20 @@ public class ALICE {
 }
 
 class AntColony {
-    public static final int MAXIMUM_ANT_AGE = 100;
-    private final int[][] flowers, regrow, limit, originalLimits, originalFlowers;
+    public static final int MAXIMUM_ANT_AGE = 7;
+    private final int[][] regrow, originalLimits, originalFlowers;
     private final double[][] pheromone;
     private final int DIMENSIONS;
-    private final int[][][] fences;
+    private final Fence[] fences;
     private final int startX;
     private final int startY;
     private final int penalty;
     private final Movement[][] MOVEMENTS;
     private final long startTime = System.currentTimeMillis();
     private final int neighbours[][][][];
-    private static final int ANT_COLONY_SIZE = 1000;
+    private static final int ANT_COLONY_SIZE = 5;
     private static final int TIME_OUT = 1000;
     private static final double evaporationCoefficient = 0.1;
-    private final long[][][] penalties;
 
     public AntColony(final int[][] originalFlowers,
                      final int[][] regrow,
@@ -72,18 +74,14 @@ class AntColony {
         this.regrow = regrow;
         this.originalLimits = originalLimits;
         this.DIMENSIONS = DIMENSIONS;
-        this.fences = fences;
+        this.fences = new Fence[fences.length];
+        for (int i = 0; i < fences.length; i++) {
+            this.fences[i] = new Fence(fences[i][0][0], fences[i][0][1], fences[i][1][0], fences[i][1][1]);
+        }
+        Arrays.sort(this.fences);
         this.startX = startX;
         this.startY = startY;
         this.penalty = penalty;
-        this.limit = new int[DIMENSIONS][DIMENSIONS];
-        this.flowers = new int[DIMENSIONS][DIMENSIONS];
-        for (int i = 0; i < DIMENSIONS; i++) {
-            System.arraycopy(this.originalLimits[i], 0, this.limit[i], 0, DIMENSIONS);
-        }
-        for (int i = 0; i < DIMENSIONS; i++) {
-            System.arraycopy(this.originalFlowers[i], 0, this.flowers[i], 0, DIMENSIONS);
-        }
         this.MOVEMENTS = new Movement[DIMENSIONS][DIMENSIONS];
         for (int i = 0; i < DIMENSIONS; i++) {
             for (int j = 0; j < DIMENSIONS; j++) {
@@ -91,50 +89,61 @@ class AntColony {
             }
         }
         neighbours = new int[DIMENSIONS][DIMENSIONS][][];
-        penalties = new long[DIMENSIONS][DIMENSIONS][];
-        findAndSetNeighboursWithCost();
-        System.out.println(Arrays.deepToString(neighbours));
-        System.out.println(Arrays.deepToString(penalties));
+        findAndSetNeighbours();
         pheromone = new double[DIMENSIONS][DIMENSIONS];
     }
 
     public Movement[] findBestTour() {
         List<Movement> bestTour = Collections.emptyList();
-        long bestReward = Integer.MIN_VALUE;
+        long bestReward = 0;
         for (int ant = 0; ant < ANT_COLONY_SIZE && System.currentTimeMillis() - startTime < TIME_OUT; ant++) {
+            final int limit[][] = new int[DIMENSIONS][DIMENSIONS];
+            final int flowers[][] = new int[DIMENSIONS][DIMENSIONS];
             int x = startX, y = startY;
             final List<Movement> tour = new ArrayList<>(MAXIMUM_ANT_AGE);
-            long reward = 0;
             final Movement[] possibleMoves = new Movement[9];
             for (int i = 0; i < DIMENSIONS; i++) {
-                System.arraycopy(this.originalLimits[i], 0, this.limit[i], 0, DIMENSIONS);
+                System.arraycopy(this.originalLimits[i], 0, limit[i], 0, DIMENSIONS);
             }
             for (int i = 0; i < DIMENSIONS; i++) {
-                System.arraycopy(this.originalFlowers[i], 0, this.flowers[i], 0, DIMENSIONS);
+                System.arraycopy(this.originalFlowers[i], 0, flowers[i], 0, DIMENSIONS);
             }
             final int touched[][] = new int[DIMENSIONS][DIMENSIONS];
+            long reward = flowers[startX][startY];
+            flowers[startX][startY] = 0;
+            touched[startX][startY] = 0;
+            limit[startX][startY]--;
             for (int age = 0; age < MAXIMUM_ANT_AGE; age++) {
                 int options = 0;
-                for (int cell = 0; cell < neighbours[x][y].length; cell++) {
-                    if (limit[neighbours[x][y][0][cell]][neighbours[x][y][1][cell]] != 0) {
-                        if (age - touched[x][y] >= regrow[x][y]) {
-                            flowers[x][y] = originalFlowers[x][y];
+                for (int cell = 0; cell < neighbours[x][y][0].length; cell++) {
+                    final int xToVisit = neighbours[x][y][0][cell], yToVisit = neighbours[x][y][1][cell];
+                    if (limit[xToVisit][yToVisit] != 0) {
+                        if (age - touched[xToVisit][yToVisit] >= regrow[xToVisit][yToVisit]) {
+                            flowers[xToVisit][yToVisit] = originalFlowers[xToVisit][yToVisit];
                         }
-                        possibleMoves[options++] = MOVEMENTS[neighbours[x][y][0][cell]][neighbours[x][y][1][cell]];
+                        possibleMoves[options++] = MOVEMENTS[xToVisit][yToVisit];
                     }
                 }
                 if (options == 0) {
                     break;
                 } else {
-                    final int moveIndex = findMove(possibleMoves, options);
-                    Movement move = possibleMoves[moveIndex];
+                    final Movement move = findMove(flowers, possibleMoves, options, x, y);
                     tour.add(move);
-                    reward = reward + flowers[move.x][move.y] - penalties[x][y][moveIndex];
+                    reward = reward + flowers[move.x][move.y] - findPenalty(x, y, move.x, move.y);
                     x = move.x;
                     y = move.y;
                     limit[x][y]--;
                     flowers[x][y] = 0;
                     touched[x][y] = age;
+                }
+            }
+            for (final Movement movement : tour) {
+                pheromone[movement.x][movement.y] +=
+                        (double) reward * flowers[movement.x][movement.y] / (DIMENSIONS * DIMENSIONS);
+            }
+            for (int i = 0; i < DIMENSIONS; i++) {
+                for (int j = 0; j < DIMENSIONS; j++) {
+                    pheromone[i][j] *= (1 - evaporationCoefficient);
                 }
             }
             if (bestReward < reward) {
@@ -145,13 +154,51 @@ class AntColony {
         return bestTour.toArray(new Movement[bestTour.size()]);
     }
 
-    private final Random random = new Random();
-
-    private int findMove(final Movement[] possibleMoves, final int options) {
-        return random.nextInt(options);
+    private long findPenalty(final int startX, final int startY, final int endX, final int endY) {
+        int index = Arrays.binarySearch(fences, new Fence(endX, endY, endX, endY));
+        if (index < 0) {
+            index = -(index + 1);
+        }
+        for (int i = index; i < fences.length && startX >= fences[i].x1; i++) {
+            final Fence fence = fences[i];
+            if (isInside(endX, endY, fence)) {
+                if (!isInside(startX, startY, fence)) {
+                    return penalty;
+                }
+            }
+        }
+        return 0;
     }
 
-    private void findAndSetNeighboursWithCost() {
+    private boolean isInside(final int x, final int y, final Fence fence) {
+        return fence.x1 <= x && x <= fence.x2 && fence.y1 <= y && y <= fence.y2;
+    }
+
+    private Movement findMove(final int[][] flowers,
+                              final Movement[] possibleMoves,
+                              final int options,
+                              final int x, final int y) {
+        double totalScore = 0;
+        final double[] score = new double[options];
+        for (int i = 0; i < options; i++) {
+            final Movement move = possibleMoves[i];
+            score[i] = Math.pow(flowers[move.x][move.y] - findPenalty(x, y, move.x, move.y), 0.8);
+            //* (1 + Math.pow(pheromone[move.x][move.y], 1.2));
+            totalScore = totalScore + score[i];
+        }
+        double roulette = Math.random();
+        for (int i = 0; i < options; i++) {
+            score[i] /= totalScore;
+            if (score[i] > roulette) {
+                return possibleMoves[i];
+            } else {
+                roulette -= score[i];
+            }
+        }
+        return possibleMoves[0];
+    }
+
+    private void findAndSetNeighbours() {
         final int temp[][] = new int[2][9];
         for (int i = 0; i < DIMENSIONS; i++) {
             for (int j = 0; j < DIMENSIONS; j++) {
@@ -200,61 +247,34 @@ class AntColony {
                     }
                 }
                 neighbours[i][j] = new int[2][nearby];
-                penalties[i][j] = new long[nearby];
                 System.arraycopy(temp[0], 0, neighbours[i][j][0], 0, nearby);
                 System.arraycopy(temp[1], 0, neighbours[i][j][1], 0, nearby);
             }
         }
-        for (final int[][] fence : fences) {
-            if (fence[0][0] > 0) {
-                final int x1 = fence[0][0] - 1;
-                final int start = fence[0][1] == 0 ? 0 : fence[0][1] - 1;
-                final int end = fence[1][1] == DIMENSIONS - 1 ? fence[1][1] : fence[1][1] + 1;
-                for (int y = start; y <= end; y++) {
-                    for (int j = 0; j < neighbours[x1][y][0].length; j++) {
-                        if (neighbours[x1][y][0][j] == fence[0][0]) {
-                            penalties[x1][y][j] = penalty;
-                        }
-                    }
-                }
-            }
-            if (fence[1][0] + 1 < DIMENSIONS) {
-                final int x2 = fence[1][0] + 1;
-                final int start = fence[0][1] == 0 ? 0 : fence[0][1] - 1;
-                final int end = fence[1][1] == DIMENSIONS - 1 ? fence[1][1] : fence[1][1] + 1;
-                for (int y = start; y <= end; y++) {
-                    for (int j = 0; j < neighbours[x2][y][0].length; j++) {
-                        if (neighbours[x2][y][0][j] == fence[1][0]) {
-                            penalties[x2][y][j] = penalty;
-                        }
-                    }
-                }
-            }
-            if (fence[0][1] > 0) {
-                final int y1 = fence[0][1] - 1;
-                final int start = fence[0][0];
-                final int end = fence[1][0];
-                for (int x = start; x <= end; x++) {
-                    for (int j = 0; j < 3; j++) {
-                        if (neighbours[x][y1][1][j] == fence[0][1]) {
-                            penalties[x][y1][j] = penalty;
-                        }
-                    }
-                }
-            }
-            if (fence[1][1] + 1 < DIMENSIONS) {
-                final int y2 = fence[1][1] + 1;
-                final int start = fence[0][0];
-                final int end = fence[1][0];
-                for (int x = start; x <= end; x++) {
-                    for (int j = neighbours[x][y2][0].length - 3; j < neighbours[x][y2][0].length; j++) {
-                        if (neighbours[x][y2][1][j] == fence[1][1]) {
-                            penalties[x][y2][j] = penalty;
-                        }
-                    }
-                }
-            }
+    }
+}
+
+class Fence implements Comparable<Fence> {
+    final int x1, y1, x2, y2;
+
+    public Fence(final int x1, final int y1, final int x2, final int y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+    @Override
+    public int compareTo(final Fence other) {
+        final int upperDiff = x1 - other.x1;
+        if (upperDiff != 0) {
+            return upperDiff;
         }
+        final int lowerDiff = x2 - other.x2;
+        if (lowerDiff != 0) {
+            return lowerDiff;
+        }
+        return y1 != other.y1 ? y1 - other.y2 : y2 - other.y2;
     }
 }
 
@@ -300,18 +320,6 @@ class InputReader {
         return buf[curChar++];
     }
 
-    public String readString() {
-        final StringBuilder stringBuilder = new StringBuilder();
-        int c = read();
-        while (isSpaceChar(c))
-            c = read();
-        do {
-            stringBuilder.append(c);
-            c = read();
-        } while (!isSpaceChar(c));
-        return stringBuilder.toString();
-    }
-
     public int readInt() {
         int c = read();
         while (isSpaceChar(c))
@@ -322,24 +330,6 @@ class InputReader {
             c = read();
         }
         int res = 0;
-        do {
-            res *= 10;
-            res += c - '0';
-            c = read();
-        } while (!isSpaceChar(c));
-        return res * sgn;
-    }
-
-    public long readLong() {
-        int c = read();
-        while (isSpaceChar(c))
-            c = read();
-        int sgn = 1;
-        if (c == '-') {
-            sgn = -1;
-            c = read();
-        }
-        long res = 0;
         do {
             res *= 10;
             res += c - '0';
