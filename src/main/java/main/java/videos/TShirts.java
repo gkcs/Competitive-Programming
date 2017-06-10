2fb21c4459f294a5b4009019280a7cb0cc5deaa9
gkcs <gauravsen92@gmail.com>
2017-06-11 02:18:35 +0530

    tshirts right answer

--- src/main/java/main/java/videos/TShirts.java
+++ src/main/java/main/java/videos/TShirts.java
@@ -11,8 +11,12 @@ public class TShirts {
         final StringBuilder stringBuilder = new StringBuilder();
         for (int t = Integer.parseInt(br.readLine()); t > 0; t--) {
             final int n = Integer.parseInt(br.readLine());
-            final int sets[] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            stringBuilder.append(new Solver(sets).solve()).append('\n');
+            final int wardrobe[][] = new int[n][];
+            for (int i = 0; i < n; i++) {
+                wardrobe[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            }
+            final Solver solver = new Solver(wardrobe);
+            stringBuilder.append(solver.solve()).append('\n');
         }
         System.out.println(stringBuilder);
     }
@@ -21,15 +25,26 @@ public class TShirts {
 class Solver {
 
     public static final int MOD = 1000000007;
-    private int[][] shirtSets;
+    private final int length;
+    private int[][] people;
+    private final int[] count;
+    private final long dp[][];
 
-    public Solver(final int[] people) {
-        final int shirtSets[][] = new int[100][people.length];
-        final int count[] = new int[people.length];
-        for (int i = 0; i < people.length; i++) {
-            shirtSets[people[i]][count[people[i]]++] = i;
+    public Solver(final int[][] wardrobe) {
+        final int people[][] = new int[101][wardrobe.length];
+        count = new int[101];
+        for (int i = 0; i < wardrobe.length; i++) {
+            for (int index = 0; index < wardrobe[i].length; index++) {
+                int shirt = wardrobe[i][index];
+                people[shirt][count[shirt]++] = i;
+            }
         }
-        this.shirtSets = shirtSets;
+        this.length = wardrobe.length;
+        this.people = people;
+        dp = new long[1 << length][101];
+        for (final long[] aDp : dp) {
+            Arrays.fill(aDp, -1);
+        }
     }
 
     public long solve() {
@@ -37,15 +52,23 @@ class Solver {
     }
 
     private long possiblities(final int people, final int assigned) {
+        if (assigned == this.people.length) {
+            return people == (1 << length) - 1 ? 1 : 0;
+        }
+        if (dp[people][assigned] != -1) {
+            return dp[people][assigned];
+        }
         long answer = 0;
-        for (int i = 0; i < shirtSets[assigned].length; i++) {
-            int person = shirtSets[assigned][i];
+        for (int i = 0; i < count[assigned]; i++) {
+            int person = this.people[assigned][i];
             if ((people & (1 << person)) == 0) {
                 int changedPeople = people;
                 changedPeople = changedPeople | (1 << person);
                 answer = (answer + possiblities(changedPeople, assigned + 1)) % MOD;
             }
         }
+        answer = (answer + possiblities(people, assigned + 1)) % MOD;
+        dp[people][assigned] = answer;
         return answer;
     }
 }
