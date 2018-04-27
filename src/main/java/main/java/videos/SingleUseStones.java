95ea9151851f473ee51e2662fd6a1e7b83940e13
gkcs <gauravsen92@gmail.com>
2018-04-27 11:18:23 +0530

    doc added

--- src/main/java/main/java/videos/SingleUseStones.java
+++ src/main/java/main/java/videos/SingleUseStones.java
@@ -4,23 +4,29 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * Codeforces Single Use Stones: https://youtu.be/5s0MRxojQQo
+ * Problem Statement: http://codeforces.com/contest/965/problem/D
+ * Two pointer sliding window solution
+ */
 public class SingleUseStones {
     public static void main(final String args[]) throws IOException {
         final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         String line[] = bufferedReader.readLine().split(" ");
-        final int w = Integer.parseInt(line[0]), l = Integer.parseInt(line[1]);
+        final int width = Integer.parseInt(line[0]), L = Integer.parseInt(line[1]);
         line = bufferedReader.readLine().split(" ");
-        final int a[] = new int[line.length];
-        for (int i = 0; i < a.length; i++) {
-            a[i] = Integer.parseInt(line[i]);
+        final int river[] = new int[line.length];
+        for (int i = 0; i < river.length; i++) {
+            river[i] = Integer.parseInt(line[i]);
         }
+        // Solution starts here
         int sum = 0;
-        for (int i = 0; i < l; i++) {
-            sum += a[i];
+        for (int i = 0; i < L; i++) {
+            sum += river[i];
         }
         int minValue = sum;
-        for (int i = l; i < w - 1; i++) {
-            sum = sum - a[i - l] + a[i];
+        for (int i = L; i < width - 1; i++) {
+            sum = sum - river[i - L] + river[i];
             minValue = Math.min(minValue, sum);
         }
         System.out.println(minValue);
